181448101353d3cc75ecf5e11b73e1c6a5250c19
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-04-17 18:55:11 -0400

    added front-end tweaks to link API calls

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -35,7 +35,7 @@ router.post("/notes", (req, res) => {
   notes.push(newNote);
 
   // TEST Notes //
-  console.log("Your created note(s) are:", notes);
+  console.log("Your created note is:", notes);
 
   // Write the file to the db.json file
   fs.writeFileSync(
@@ -56,19 +56,18 @@ router.get("/notes", (req, res) => {
 
   // Set response equal to JSON(results)
   res.sendFile(path.join(__dirname, "../../db/db.json"));
-  console.log("Data from db.json has loaded to the page!");
 });
 
 // EXTRA CREDIT //
 // TO DO: SET UP DELETE
-// router.delete("/notes/id:", (req, res) => {
+router.delete("/notes/id:", (req, res) => {
 // First - need to read all notes from the db.json file
-// res.sendFile(path.join(__dirname, "../../db/db.json"));
+res.sendFile(path.join(__dirname, "../../db/db.json"));
 // Second - remove the note with the given id property
-// var deletedNote = req.params.id
-// if(deletedNote){res.json()}
+var deletedNote = req.params.id
+if(deletedNote){res.json()}
 // Third, re-write the notes to the db.json file
-// });
+});
 
 // Export Router
 module.exports = router;
