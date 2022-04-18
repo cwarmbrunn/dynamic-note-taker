762472bec8af3f1fcc4e7be6cff0aedeb3b655f0
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-04-17 21:17:01 -0400

    completed delete functionality and confirmed MVP

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -60,28 +60,38 @@ router.get("/notes", (req, res) => {
 });
 
 // EXTRA CREDIT //
-router.delete("/notes/id:", (req, res) => {
+router.delete("/notes/:id", (req, res) => {
   // Set a variable up to capture the deleted note id
   let deletedNote = req.params.id;
   // First - need to read all notes from the db.json file
-  fs.readFile(__dirname, "/db/db.json", (data) => {
+  fs.readFile("../../db/db.json", (data) => {
     // Create a for loop to cycle through the length of existing notes
     for (let i = 0; i < notes.length; i++) {
       // Set up a conditional for if a note equals the id selected (?)
       if (notes[i].id === deletedNote) {
+        console.log(`Deleted note: ${notes[i].id} successfully!`);
         // Splice the notes and remove ONE!
         notes.splice(i, 1);
       }
     }
     // Finally, re-write the notes to the db.json file
-    fs.writeFile(__dirname + "db/db.json", JSON.stringify(notes), (err) => {
-      if (err) {
-        console.log(err);
-        res.sendStatus(500);
-        return;
+    fs.writeFile(
+      path.join(__dirname, "../../db/db.json"),
+      JSON.stringify(notes),
+      (err) => {
+        // Set up conditional if there's an error
+        if (err) {
+          // Console log the error
+          console.log(err);
+          // Send response of Status 500
+          res.sendStatus(500);
+          return;
+        }
+
+        // Send response that the note was deleted successfully
+        res.send("Deleted note successfully!");
       }
-      res.send("Deleted note successfully!");
-    });
+    );
   });
 });
 
